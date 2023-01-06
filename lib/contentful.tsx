a97c79911684f88eec9e6d98ba2e022ff8e15d1e
James <james.ide775@gmail.com>
2023-01-06 16:39:20 +1030

    rename and static path gen for records

--- lib/contentful.tsx
+++ lib/contentful.tsx
@@ -1,5 +1,6 @@
 import contentful from "contentful";
 import { createClient } from "contentful";
+import { IThumbnail } from "../@types/generated/contentful";
 // Create client and export it for use
 
 export const client = createClient({
@@ -12,4 +13,25 @@ export const getContentfulEntries = async (contentType: string) => {
     content_type: contentType,
   });
   return entries;
+};
+
+export const getRecordSlugs = async () => {
+  const entries = await client.getEntries({
+    content_type: "thumbnail",
+  });
+  return entries.items.map((entry: any) => {
+    return {
+      params: {
+        slug: entry.fields.slug,
+      },
+    };
+  });
+};
+
+export const getRecordBySlug = async (slug: string) => {
+  const record = await client.getEntries({
+    content_type: "thumbnail",
+    "fields.slug": slug,
+  });
+  return record.items[0] as IThumbnail;
 };
