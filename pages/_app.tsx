699b57731a429547e69342dbb944ce0d9c520b47
James <james.ide775@gmail.com>
2023-01-08 09:44:15 +1030

    Inject GA script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { GoogleAnalytics } from "nextjs-google-analytics";
+import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GoogleAnalytics
-        trackPageViews
-        gaMeasurementId={process.env.NEXT_PUBLIC_GOOGLE_GTAG}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-GHS0468GG6"
+        strategy="afterInteractive"
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-GHS0468GG6');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
