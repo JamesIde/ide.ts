1f12bfbd797ec1c5935f33e99f82b209667028ff
James <james.ide775@gmail.com>
2023-01-15 13:41:48 +1030

    Revert token exp times

--- lib/jwt/auth.ts
+++ lib/jwt/auth.ts
@@ -16,7 +16,7 @@ export function generateAccessToken(profile: IdpUser) {
   };
 
   return jwt.sign(ATPayload, process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET, {
-    expiresIn: "10s",
+    expiresIn: "15min",
   });
 }
 
@@ -32,7 +32,7 @@ export function generateRefreshToken(profile: IdpUser) {
   };
 
   return jwt.sign(RTPayload, process.env.NEXT_PUBLIC_REFRESH_TOKEN_SECRET, {
-    expiresIn: "7d",
+    expiresIn: "30d",
   });
 }
 
