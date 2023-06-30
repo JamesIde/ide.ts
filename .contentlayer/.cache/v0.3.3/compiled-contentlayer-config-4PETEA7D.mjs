047ade0cfba1157244079d849dc6b7530e234e5e
James <james.ide775@gmail.com>
2023-06-30 19:12:41 +0930

    Include contentlayer

---
+++ .contentlayer/.cache/v0.3.3/compiled-contentlayer-config-4PETEA7D.mjs
@@ -0,0 +1,47 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypePrettyCode from "rehype-pretty-code";
+
+// lib/code-styling/rehype.ts
+var rehypePrettyCodeOptions = {
+  theme: "one-dark-pro"
+};
+
+// contentlayer.config.ts
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `projects/**/*.mdx`,
+  // This is the location of the mdx files
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      description: "The title of the post",
+      required: true
+    },
+    date: {
+      type: "string",
+      description: "The date of the post",
+      required: true
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      // Replaces the .mdx with nothing to form the slug
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "data",
+  documentTypes: [Post],
+  mdx: {
+    rehypePlugins: [[rehypePrettyCode, rehypePrettyCodeOptions]]
+  }
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-4PETEA7D.mjs.map
