ef117b0e1e74a9a572521c647f19a347ee723637
James <james.ide775@gmail.com>
2025-05-02 21:01:53 +0930

    Cleanup

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
-import { HiExternalLink } from "react-icons/hi";
+import { MdOutlineMail } from "react-icons/md";
+import { FaInstagram } from "react-icons/fa";
+
 function Navigation({ color }: { color: string }) {
   return (
     <div
-      className="navbar  mb-2"
+      className="navbar mb-2"
       style={{
         color: color,
       }}
     >
-      <div className="container mx-auto ">
+      <div className="container mx-auto">
         <div className="flex-1">
           <div className="flex-none text-center nav-center mb-2">
             <Link href="/">
@@ -18,38 +20,43 @@ function Navigation({ color }: { color: string }) {
           <div className="lg:w-[900px] md:w-[600px] mx-auto">
             <hr />
           </div>
-
           <div className="flex justify-center mt-1 font-mono">
-            <Link
-              href="/about"
-              className="text-base hover:text-sky-700 duration-500 lg:mr-3 md:mr-3 sm:mr-3 mr-3"
-            >
+            <Link href="/about" className="text-base hover:text-sky-700 duration-500 lg:mr-3 md:mr-3 sm:mr-3 mr-3">
               ABOUT
             </Link>
-            {/* <Link
-              href="/projects"
-              className="text-base hover:text-sky-700 duration-500 lg:mr-3 md:mr-3 sm:mr-3 mr-3"
-            >
-              PROJECTS
-            </Link> */}
-            <Link
-              href="/gear"
-              className="text-base hover:text-sky-700 duration-500 lg:mr-3 md:mr-3 sm:mr-3 mr-3"
-            >
+            <Link href="/gear" className="text-base hover:text-sky-700 duration-500 lg:mr-3 md:mr-3 sm:mr-3 mr-3">
               GEAR
             </Link>
-            <a
-              href="mailto:james.ide775@gmail.com"
-              target="_#"
-              className="text-base hover:text-sky-700 duration-500 lg:mr-3 md:mr-3 sm:mr-3 mr-3 p-0"
-            >
-              CONTACT
-              <HiExternalLink size="0.7rem" className="inline-block" />
-            </a>
+            {/* <div className="pr-3">
+              <p>|</p>
+            </div>
+            <div>
+              <a
+                href="mailto:james.ide775@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-base hover:text-sky-700 duration-500 lg:mr-3 md:mr-3 sm:mr-3 mr-3 p-0"
+                aria-label="Email"
+              >
+                <MdOutlineMail className="inline-block text-lg hover:text-sky-700 duration-500" size={20} />
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://www.instagram.com/james.ide/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-base hover:text-sky-700 duration-500 p-0"
+                aria-label="Instagram"
+              >
+                <FaInstagram className="inline-block text-lg hover:text-sky-700 duration-500" />
+              </a>
+            </div> */}
           </div>
         </div>
       </div>
     </div>
   );
 }
+
 export default Navigation;
