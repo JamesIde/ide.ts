5c305edfbaf70ad93a7229f37e24abfcf07150f1
James <james.ide775@gmail.com>
2025-09-21 09:26:06 +0930

    Further cleanup

--- pages/api/records.ts
+++ pages/api/records.ts
@@ -1,13 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import {
-  retrieveRecordComments,
-  updateRecordViewCount,
-} from "../../services/records.service";
+import { updateRecordViewCount } from "../../services/records.service";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET" && req.query.contentfulId) {
-    return retrieveRecordComments(req, res);
-  } else if (req.method === "POST" && req.query.contentfulId) {
+  if (req.method === "POST" && req.query.contentfulId) {
     return updateRecordViewCount(req, res);
   } else {
     return res.status(405).send("Method not allowed");
--- services/records.service.ts
+++ services/records.service.ts
@@ -1,62 +1,10 @@
 import prisma from "../config/prisma";
-import createNestedStructure from "../lib/transformer/nestedComment";
-import { Comment } from "../@types/Comment";
 import { NextApiRequest, NextApiResponse } from "next";
-import * as Sentry from "@sentry/nextjs";
-/**
- * A public method to retrieve all comments for a record
- */
-export async function retrieveRecordComments(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const contentfulId = req.query.contentfulId as string;
-  try {
-    const rootComments = await prisma.comment.findMany({
-      where: {
-        record: {
-          id: contentfulId,
-        },
-      },
-      include: {
-        user: {
-          select: {
-            id: true,
-            name: true,
-            picture: true,
-          },
-        },
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    Sentry.captureMessage(`Retrieved comments for record: ${contentfulId}`);
-    const commentTree = createNestedStructure(rootComments as Comment[]);
-    return res.status(200).json({
-      commentCount: rootComments.length,
-      comments: commentTree,
-    });
-  } catch (error) {
-    console.log(`HERE`, error);
-    Sentry.captureException(error, {
-      tags: {
-        contentfulId,
-      },
-    });
-    return res.status(400).json({
-      message: `Error retrieving comments for record: ${contentfulId}`,
-    });
-  }
-}
 
 /**
  * A public method to update the view count for a record
  */
-export async function updateRecordViewCount(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function updateRecordViewCount(req: NextApiRequest, res: NextApiResponse) {
   const contentfulId = req.query.contentfulId as string;
   try {
     const record = await prisma.record.findUnique({
@@ -73,18 +21,11 @@ export async function updateRecordViewCount(
         viewCount: record.viewCount + 1,
       },
     });
-    Sentry.captureMessage(`Adjusted view count for record: ${contentfulId}`);
 
     return res.status(200).json({
       viewCount: updatedRecord.viewCount,
     });
   } catch (error) {
-    console.log(`HERE`, error);
-    Sentry.captureException(error, {
-      tags: {
-        contentfulId,
-      },
-    });
     return res.status(400).json({
       message: `Error updating view count for record: ${contentfulId}`,
     });
