92cf6bb2f2941018910b391bd8877226a68c4a89
James <james.ide775@gmail.com>
2025-10-03 08:27:04 +0930

    Set cursor pointer when hovering over image

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 import { ModalImage } from "../../@types/Modal";
 
 export default function Modal({ currImage, id, onClose }: { currImage: ModalImage; id: string; onClose: () => void }) {
+  console.log(currImage);
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-      <div className="relative max-w-[90vw]">
+      <div className="relative max-w-[90vw] hover:cursor-pointer">
         <Image
           src={currImage.url}
           alt={id}
           width={parseInt(currImage.width)}
           height={parseInt(currImage.height)}
           className={"max-w-full max-h-[90vh] object-contain transition-opacity duration-300 opacity-100"}
-          quality={100}
+          quality={75}
           priority
         />
         <button onClick={onClose} className="absolute top-2 right-2 text-white bg-black bg-opacity-50 rounded-full p-2">
