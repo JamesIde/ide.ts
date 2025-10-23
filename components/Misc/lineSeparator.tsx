882de56901b788fdd4aa1a9847c4d63dcdd3d8b4
James Ide <james.ide775@gmail.com>
2025-10-24 08:24:28 +1030

    Minor layout changes (#72)

--- components/Misc/lineSeparator.tsx
+++ components/Misc/lineSeparator.tsx
@@ -1,6 +1,6 @@
-function LineSeparator({ name }: { name: string }) {
+function LineSeparator({ name }: { name?: string }) {
   return (
-    <div className="flex items-center gap-4 mb-4 md:pl-0">
+    <div className={`flex items-center mb-4 md:pl-0 ${name ? "gap-4" : ""}`}>
       <div className="text-xs font-mono uppercase tracking-wider">{name}</div>
       <div className="h-px flex-1 bg-border bg-black" />
     </div>
