6451c357d9e57b93b4dfd8ec5a297ec41756c7b4
James <james.ide775@gmail.com>
2025-10-26 18:07:13 +1030

    Update about

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,18 +11,17 @@ function about() {
         <Helmet title="About" />
         <div className="xl:w-5/12 lg:w-6/12 md:w-10/12 mx-auto xl:mt-32 p-4 md:p-2">
           <h3 className="text-md font-semibold mb-4 tracking-wider">About</h3>
-
           <p className="text-gray-600 mt-3 leading-tight">
-            As I’ve gotten older (and not to be dramatic), I’ve realised that it is quite important to me that I have
-            some medium to collate and organise my trips and photos and share them in some way. Partly for my future
-            self to look back on but also to put something as authentic as possible out into the world. I also have
-            found that writing about trips after they have happened has been a great way to internalise and process
-            them, beyond relying on photos on a phone or a hazy recollection of memories in the future.
+            As I’ve gotten older (and not to be dramatic), I’ve realised the importance of a medium to collate and
+            organise my trips and photos to be shared in some way. Partly for my future self to look back on as a kind
+            of logbook, but also to put something as authentic as possible out to the world. I have also found that
+            writing about events after they have happened has been a method of internalising and processing them, beyond
+            relying on photos on a phone or a hazy recollection of memories.
           </p>
           <p className="text-gray-600 mt-3 leading-tight">
-            It is a small site that doesn’t get many views, and I certainly do not promote it anywhere, but if you have
-            found this in your search for knowledge about a particular trip I have done then perhaps there is something
-            of value here for you.
+            This is a small website that does not get a lot of views, and I certainly do not promote it anywhere, but if
+            you are here in your search for knowledge about a particular trip I have done then perhaps there is
+            something of value here for you.
           </p>
           <p className="text-gray-600 mt-3 leading-tight">
             If you have something to say, you can get in touch with me.
